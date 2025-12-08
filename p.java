d5f70e6a208bf0006544c4d65ef4b213c7c44cfa
SmruthiRavindra <smruthiravindra2006@gmail.com>
2025-12-08 12:45:35 +0530

    Update p.java

--- p.java
+++ p.java
@@ -1,7 +1,5 @@
-public class Hello
-  {
-    public static void main(String args)
-    {
-      System.out.println("hello");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
     }
-  }
+}
